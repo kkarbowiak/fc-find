ea542b0fc69695532c76ee1a0cb4281d60d0568d
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2018-10-23 21:24:56 +0200

    Refactoring extract code to function

--- fcf.py
+++ fcf.py
@@ -5,11 +5,7 @@ import hashlib
 
 
 def main():
-    parser = argparse.ArgumentParser(description='File duplicate finder')
-
-    parser.add_argument('source', help='source path')
-    parser.add_argument('target', help='target path')
-
+    parser = get_args_parser()
     args = parser.parse_args()
 
     source_data = get_files_hashes(args.source)
@@ -21,6 +17,15 @@ def main():
 
     for ih in intersection_hashes:
         print(ih[0:8], source_data[ih], ' -> ', target_data[ih])
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(description='File duplicate finder')
+
+    parser.add_argument('source', help='source path')
+    parser.add_argument('target', help='target path')
+
+    return parser
 
 
 def get_files_hashes(path):
